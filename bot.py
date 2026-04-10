ca24e14a1a40ae58a5496ce32b00923de1416e9b
luckasttonn-ai <luckasttonn@gmail.com>
2026-04-10 15:55:35 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import requests
 import json
 import sys
 
-TOKEN = 8765865198:AAEYyQc41t2dL4eHiPs1wYMROcwtphT8x50
+TOKEN = "8765865198:AAEYyQc41t2dL4eHiPs1wYMROcwtphT8x50"
 CHAT_ID = -1003853792884
 
 # horário do post
