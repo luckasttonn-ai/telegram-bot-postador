d34a059c7782db4e29f22b7c323fcf75ecca94c9
luckasttonn-ai <luckasttonn@gmail.com>
2026-04-10 16:24:26 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,36 @@
 import requests
+import json
 
 TOKEN = "8765865198:AAEYyQc41t2dL4eHiPs1wYMROcwtphT8x50"
 CHAT_ID = "-1003853792884"
 
-mensagem = "🚀 TESTE DO BOT FUNCIONANDO!"
+with open("posts.json", "r", encoding="utf-8") as file:
+    posts = json.load(file)
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+post = posts[0]
+
+mensagem = f"""
+🔥 OFERTA DO DIA!
+
+🛍 {post['produto']}
+
+💰 De: {post['preco_antigo']}
+💸 Por: {post['preco_novo']}
+
+{post['descricao']}
+
+⚠️ Promoção por tempo limitado!
+
+🔗 COMPRE AQUI
+{post['link']}
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
 
 payload = {
     "chat_id": CHAT_ID,
-    "text": mensagem
+    "caption": mensagem,
+    "photo": post["imagem"]
 }
 
 requests.post(url, data=payload)
