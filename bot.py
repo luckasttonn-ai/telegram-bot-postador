ad707ad3a472b8e75471110ea4d60c602943c95b
luckasttonn-ai <luckasttonn@gmail.com>
2026-04-10 16:19:01 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,15 @@
 import requests
-import json
-import sys
 
 TOKEN = "8765865198:AAEYyQc41t2dL4eHiPs1wYMROcwtphT8x50"
 CHAT_ID = "-1003853792884"
 
-# horário do post
-post_number = int(sys.argv[1])
+mensagem = "🚀 TESTE DO BOT FUNCIONANDO!"
 
-with open("posts.json", "r") as f:
-    posts = json.load(f)
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-post = posts[post_number]
-
-url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
-
-data = {
+payload = {
     "chat_id": CHAT_ID,
-    "photo": post["imagem"],
-    "caption": post["texto"],
-    "parse_mode": "HTML"
+    "text": mensagem
 }
 
-requests.post(url, data=data)
+requests.post(url, data=payload)
